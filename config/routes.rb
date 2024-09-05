81bfc361ad1136aa98fc4e602420b783012b984a
guswhitten <whittenoswald@gmail.com>
2024-09-05 06:21:58 -0400

    add privacy page and add footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/privacy'
   root 'home#index'
 
   resources :games
   post '/games/:id/bot_buzzed', to: 'games#bot_buzzed'
+  get 'privacy', to: 'static_pages#privacy'
 end
